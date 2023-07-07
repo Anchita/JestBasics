a31d13ad385468bfd75880d5f882f905e7e6181f
Abhinav Arora <aroras@abhinavs-imac.lan>
2023-07-07 14:49:22 +0100

    added test for floating point numbers using toBeCloseTo matcher

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,9 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1,2)).toBe(3);
 });
+
+test('adding floating point numbers', ()=> {
+const value = 0.1 + 0.2;
+
+expect(value).toBeCloseTo(0.3);
+});
